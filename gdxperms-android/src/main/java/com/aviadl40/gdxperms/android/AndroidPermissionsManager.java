2631b638a8d139f2d27d0d635735883d5afa7b62
ExoSkeleton <calazulazu@gmail.com>
2023-01-03 13:16:55 +0200

    Added permission requirement map for different android versions Implemented permission request listening

--- gdxperms-android/src/main/java/com/aviadl40/gdxperms/android/AndroidPermissionsManager.java
+++ gdxperms-android/src/main/java/com/aviadl40/gdxperms/android/AndroidPermissionsManager.java
@@ -4,31 +4,58 @@ import android.Manifest;
 import android.content.pm.PackageManager;
 import android.os.Build;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 
-import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.aviadl40.gdxperms.core.PermissionsManager;
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.utils.Array;
+
+import java.util.HashMap;
 
 public final class AndroidPermissionsManager implements PermissionsManager {
+	private static final int REQ_PERMS = 7656;
+
 	private final AndroidApplication mAndroid;
+	private final HashMap<Permission, Array<String>> permRequestStringsMap = new HashMap<>();
+	private PermissionRequestListener mPermRequestListener = null;
 
 	public AndroidPermissionsManager(AndroidApplication mAndroid) {
 		this.mAndroid = mAndroid;
+
+		// Skip perm request map setup as perms do not need requesting in this and prior versions
+		if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M) return;
+
+		Array<String> baseBluetoothPermStrings = Array.with(Manifest.permission.BLUETOOTH, Manifest.permission.BLUETOOTH_ADMIN);
+		if (Build.VERSION.SDK_INT < Build.VERSION_CODES.S) {
+			baseBluetoothPermStrings.addAll(
+					Manifest.permission.ACCESS_FINE_LOCATION,
+					Manifest.permission.ACCESS_COARSE_LOCATION
+			);
+
+			permRequestStringsMap.put(Permission.BLUETOOTH_ADVERTISE, baseBluetoothPermStrings);
+			permRequestStringsMap.put(Permission.BLUETOOTH_SCAN, baseBluetoothPermStrings);
+			permRequestStringsMap.put(Permission.BLUETOOTH_CONNECT, baseBluetoothPermStrings);
+		} else {
+			permRequestStringsMap.put(Permission.BLUETOOTH_ADVERTISE, Array.with(Manifest.permission.BLUETOOTH_ADVERTISE));
+			permRequestStringsMap.put(Permission.BLUETOOTH_SCAN, Array.with(
+					Manifest.permission.BLUETOOTH_SCAN,
+					Manifest.permission.BLUETOOTH_CONNECT
+			));
+			permRequestStringsMap.put(Permission.BLUETOOTH_CONNECT, Array.with(Manifest.permission.BLUETOOTH_CONNECT));
+		}
+		permRequestStringsMap.put(Permission.BLUETOOTH, baseBluetoothPermStrings);
+
+		permRequestStringsMap.put(Permission.LOCATION_FINE, Array.with(Manifest.permission.ACCESS_FINE_LOCATION));
+		permRequestStringsMap.put(Permission.LOCATION_COARSE, Array.with(Manifest.permission.ACCESS_COARSE_LOCATION));
 	}
 
-	private String getPermName(PermissionsManager.Permission perm) {
-		switch (perm) {
-			case BLUETOOTH:
-				return Manifest.permission.BLUETOOTH;
-			case BLUETOOTH_ADMIN:
-				return Manifest.permission.BLUETOOTH_ADMIN;
-			case LOCATION_COARSE:
-				return Manifest.permission.ACCESS_COARSE_LOCATION;
-			case LOCATION_FINE:
-				return Manifest.permission.ACCESS_FINE_LOCATION;
-			case INTERNET:
-				return Manifest.permission.INTERNET;
-			default:
-				return null;
+	public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+		if (requestCode == REQ_PERMS && mPermRequestListener != null) {
+			boolean grantedAll = true;
+			for (int i = 0; i < permissions.length && grantedAll; i++)
+				grantedAll = grantResults[i] == PackageManager.PERMISSION_GRANTED;
+			if (grantedAll) mPermRequestListener.OnGranted();
+			else mPermRequestListener.OnDenied();
 		}
 	}
 
@@ -38,24 +65,26 @@ public final class AndroidPermissionsManager implements PermissionsManager {
 			return true;
 		if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M)
 			return true;
-		String name;
+		Array<String> permStrings;
 		for (Permission perm : permissions) {
-			name = getPermName(perm);
-			if (name != null && mAndroid.checkSelfPermission(name) != PackageManager.PERMISSION_GRANTED)
-				return false;
+			permStrings = permRequestStringsMap.get(perm);
+			if (permStrings != null)
+				for (String permString : permStrings)
+					if (permString != null)
+						if (mAndroid.checkSelfPermission(permString) != PackageManager.PERMISSION_GRANTED)
+							return false;
 		}
 		return true;
 	}
 
 	@Override
-	public void requestPermissions(@NonNull Permission... permissions) {
-		if (permissions.length == 0)
-			return;
-		if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M)
-			return;
-		final String[] names = new String[permissions.length];
-		for (int i = 0; i < permissions.length; i++)
-			names[i] = getPermName(permissions[i]);
-		mAndroid.requestPermissions(names, 42);
+	public void requestPermissions(@NonNull Permission permission, @Nullable PermissionRequestListener requestListener) {
+		if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M) return;
+
+		mPermRequestListener = requestListener;
+
+		Array<String> permStrings = permRequestStringsMap.get(permission);
+		if (permStrings != null)
+			mAndroid.requestPermissions(permStrings.toArray(String.class), REQ_PERMS);
 	}
 }
